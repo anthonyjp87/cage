ab010d77da40b02bf567befb4750270a0d84c5d3
anthonyjp87 <john.a.perez@gmail.com>
2016-02-04 19:53:45 +0000

    Cleaned up stuff, fixed timer, and formatting housekeeping

--- cage.py
+++ cage.py
@@ -1,9 +1,6 @@
-import numpy as np
-import random
-import csv
-import time
-import sys
+import numpy as np, random, csv, time, sys
 
+##Import Data from CSV Files
 f = open('cagenum.csv')
 csv_f = csv.reader(f)
 a = []
@@ -20,29 +17,19 @@ for row in csv_w:
 	w.append(row)
 
 
+#Set up trivial Variables 
 N_word = len(w)
 N_num = len(a)
 count = 0
 
-#collecting the number fo player
+
+#Creating Player Array
 uNumberofPlayers = int(raw_input("Please enter number of players "))
-# try:
-#     number = int(uNumberofPlayers)
-# except ValueError:
-#     print("Invalid number")	
-#     sys.exit(1)
-#comment
-
-#creating player array
-
-
-
 uPlayerName = []
 for i in xrange(0, uNumberofPlayers):
 	uPlayerName.append(raw_input("Enter a name:"))
 
-#the game engine
-
+#Game Engine
 while (count < N_word):
 	print "Player: ", uPlayerName[count%uNumberofPlayers]
 	wait_sec = a[random.randrange(1,N_num,1)]
@@ -54,8 +41,7 @@ while (count < N_word):
 
 	if gameType_itt == "Seconds":
 		for i in xrange(wait_sec_int, 0, -1):
-			time.sleep(1)
 			print i
-		
+			time.sleep(1)
 	count = count +1
 	print "DONE"
